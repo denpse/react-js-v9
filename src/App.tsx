3ad6d56ce58ad002d36b43ab165b1e2dde227770
DenSovan <sarimramata@gmail.com>
2025-03-31 16:12:48 +0700

    typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,32 @@
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
+
+type userType = {
+  userId: string;
+  title: string;
+  body: string;
+  id: string;
+};
 
 const App = () => {
-  const [count, setCount] = useState(1); // init state
+  const [datas, setData] = useState<userType[]>([]); //typescript from userType
+  //we use this useEffect for fetching api or side effects
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
 
-  //function for increment state (count)
-  const inCrement = () => {
-    setCount(count + 1);
-  };
   return (
     <div>
-      <h1 className="text-red-900 font-bold text-xl">This is count</h1>
-      <h1 className="text-red-500">{count}</h1>
-      <button onClick={inCrement}>Increment</button>
+      <h1>Hello fetch api</h1>
+      {datas.map((res) => {
+        return (
+          <div>
+            <div>{res.title}</div>
+            <div>{res.body}</div>
+          </div>
+        );
+      })}
     </div>
   );
 };
